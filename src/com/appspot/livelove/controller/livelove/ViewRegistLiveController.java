7cac1643c8ba9e25ee3ccd33a4de901cb040c976
georgenano <okada_george@kcf.biglobe.ne.jp>
2011-08-28 12:36:09 +0900

    refactoring: refactoring jsp-file

--- src/com/appspot/livelove/controller/livelove/ViewRegistLiveController.java
+++ src/com/appspot/livelove/controller/livelove/ViewRegistLiveController.java
@@ -1,5 +1,7 @@
 package com.appspot.livelove.controller.livelove;
 
+import java.util.Calendar;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
@@ -7,6 +9,16 @@ public class ViewRegistLiveController extends Controller {
 
     @Override
     public Navigation run() throws Exception {
+        Calendar cal = Calendar.getInstance();
+        int thisYear = cal.get(Calendar.YEAR);
+        int thisMonth = cal.get(Calendar.MONTH) + 1;
+        int thisDay = cal.get(Calendar.DATE);
+        int dayMaximum = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        requestScope("thisYear", thisYear);
+        requestScope("thisMonth", thisMonth);
+        requestScope("thisDay", thisDay);
+        requestScope("dayMaximum", dayMaximum);
+
         return forward("viewRegistLive.jsp");
     }
 }
