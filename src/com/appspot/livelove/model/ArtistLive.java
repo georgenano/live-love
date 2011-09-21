d027d5399cc1c1c12e288b3ba2e3f73a99d8e2aa
georgenano <okada_george@kcf.biglobe.ne.jp>
2011-09-21 20:46:17 +0900

    commit omission

---
+++ src/com/appspot/livelove/model/ArtistLive.java
@@ -0,0 +1,123 @@
+package com.appspot.livelove.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.google.appengine.api.datastore.Key;
+
+import org.slim3.datastore.Attribute;
+import org.slim3.datastore.Model;
+import org.slim3.datastore.ModelRef;
+
+@Model(schemaVersion = 1)
+public class ArtistLive implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Attribute(primaryKey = true)
+    private Key key;
+
+    @Attribute(version = true)
+    private Long version;
+
+    private ModelRef<Artist> artistRef = new ModelRef<Artist>(
+        Artist.class);
+
+    private ModelRef<Live> liveRef = new ModelRef<Live>(Live.class);
+
+    private Date registDate;
+
+    private boolean isDeleted;
+
+    /**
+     * Returns the key.
+     *
+     * @return the key
+     */
+    public Key getKey() {
+        return key;
+    }
+
+    /**
+     * Sets the key.
+     *
+     * @param key
+     *            the key
+     */
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    /**
+     * Returns the version.
+     *
+     * @return the version
+     */
+    public Long getVersion() {
+        return version;
+    }
+
+    /**
+     * Sets the version.
+     *
+     * @param version
+     *            the version
+     */
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Date getRegistDate() {
+        return registDate;
+    }
+
+    public void setRegistDate(Date registDate) {
+        this.registDate = registDate;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public ModelRef<Artist> getArtistRef() {
+        return artistRef;
+    }
+
+    public ModelRef<Live> getLiveRef() {
+        return liveRef;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((key == null) ? 0 : key.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ArtistLive other = (ArtistLive) obj;
+        if (key == null) {
+            if (other.key != null) {
+                return false;
+            }
+        } else if (!key.equals(other.key)) {
+            return false;
+        }
+        return true;
+    }
+}
